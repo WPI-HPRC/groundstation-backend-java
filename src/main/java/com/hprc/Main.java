686ff65dfd179714ae186cab6b791bcbcb14f09a
Daniel P <dpear0915@gmail.com>
2022-03-22 00:05:42 -0400

    Added native branch

--- src/main/java/com/hprc/Main.java
+++ src/main/java/com/hprc/Main.java
@@ -11,31 +11,8 @@ import java.util.*;
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger("Groundstation");
 
-    private static SerialManager serialManager = new SerialManager();
-
     public static void main(String[] args) throws IOException {
         logger.info("Starting Backend...");
-
-        //Serial Manager Configuration
-        serialManager.setBaudRate(9600);
-        serialManager.setPacketSize(22);
-
-        serialManager.addIdentifier(new ArrayList<>(Arrays.asList(1,2,3,4)), "Altitude", DataTypes.FLOAT);
-        serialManager.addIdentifier(new ArrayList<>(Arrays.asList(4,3,2,1)), "Velocity", DataTypes.FLOAT);
-        serialManager.addIdentifier(new ArrayList<>(Arrays.asList(69,78,68,66)), "EndB", DataTypes.END_BYTES);
-        serialManager.addIdentifier(new ArrayList<>(Arrays.asList(65,67,88)), "AccelX", DataTypes.SIGNED_INT);
-        serialManager.addIdentifier(new ArrayList<>(Arrays.asList(65,67,89)), "AccelY", DataTypes.SIGNED_INT);
-        serialManager.addIdentifier(new ArrayList<>(Arrays.asList(65,67,90)), "AccelZ", DataTypes.SIGNED_INT);
-        serialManager.addIdentifier(new ArrayList<>(Arrays.asList(71,89,88)), "GyroX", DataTypes.SIGNED_INT);
-        serialManager.addIdentifier(new ArrayList<>(Arrays.asList(71,89,89)), "GyroY", DataTypes.SIGNED_INT);
-        serialManager.addIdentifier(new ArrayList<>(Arrays.asList(71,89,90)), "GyroZ", DataTypes.SIGNED_INT);
-        serialManager.addIdentifier(new ArrayList<>(Arrays.asList(65,76,84)), "Altitude", DataTypes.SIGNED_INT);
-        serialManager.addIdentifier(new ArrayList<>(Arrays.asList(83,84,84)), "State", DataTypes.SIGNED_INT);
-        serialManager.addIdentifier(new ArrayList<>(Arrays.asList(84,83,80)), "Timestamp", DataTypes.SIGNED_INT);
-        serialManager.addIdentifier(new ArrayList<>(Arrays.asList(84,77,80)), "Temperature", DataTypes.FLOAT);
-        //Open com port and start serial stream
-        serialManager.startStream();
-        serialManager.logInfo();
 
     }
 }
--- src/main/java/com/hprc/serial/DataTypes.java
+++
@@ -1,5 +0,0 @@
-package com.hprc.serial;
-
-public enum DataTypes {
-    FLOAT, UNSIGNED_INT, SIGNED_INT, END_BYTES
-}
