d5b2e27deffe6933d130ad3d01e688ba791b892b
Daniel Pearson <dpear0915@gmail.com>
2023-10-01 18:59:52 -0400

    Fixed simulation data reader

--- src/main/java/com/hprc/serial/SerialManager.java
+++ src/main/java/com/hprc/serial/SerialManager.java
@@ -13,6 +13,7 @@ import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @SuppressWarnings({"unused", "rawtypes", "SuspiciousMethodCalls"})
@@ -153,78 +154,40 @@ public class SerialManager implements SerialPortEventListener {
 
                     int index = 0;
 
-                    wss.start();                
+                    wss.start();
+
+                    while(wss.getConnections().size() < 1);
                     while(simScanner.hasNextLine()) {
 
                         String nextL = simScanner.nextLine();
                         telemetry.clear();
                         String[] lineArray = nextL.split(",");
-                    
-                        try {
-                            for(int colIndex = 0; colIndex < idArray.length; colIndex++) {
-                                switch(idArray[colIndex]) {
-                                    case "AccelX":
-                                        int accel_X = Integer.parseInt(lineArray[colIndex]);
-                                        telemetry.put("AccelX", accel_X);
-                                        break;
-                                    case "AccelY":
-                                        int accel_Y = Integer.parseInt(lineArray[colIndex]);
-                                        telemetry.put("AccelZ", accel_Y);
-                                        break;
-                                    case "AccelZ":
-                                        int accel_Z = Integer.parseInt(lineArray[colIndex]);
-                                        telemetry.put("AccelY", accel_Z);
-                                        break;
-                                    case "GyroX":
-                                        int gyro_X = Integer.parseInt(lineArray[colIndex]);
-                                        telemetry.put("GyroX", gyro_X); // these are flipped for testing
-                                        break;    
-                                    case "GyroY":
-                                        int gyro_Y = Integer.parseInt(lineArray[colIndex]);
-                                        telemetry.put("GyroY", gyro_Y); // 
-                                        break;
-                                    case "GyroZ":
-                                        int gyro_Z = Integer.parseInt(lineArray[colIndex]);
-                                        telemetry.put("GyroZ", gyro_Z); // its on purpose, trust me!
-                                        break;    
-                                    case "Altitude":
-                                        float altitude = Float.parseFloat(lineArray[colIndex]);
-                                        telemetry.put("Altitude", altitude);
-                                        break;    
-                                    case "Temperature":
-                                        float temperature = Float.parseFloat(lineArray[colIndex]);
-                                        telemetry.put("Temperature", temperature);
-                                        break;
-                                    case "State":
-                                        int state = Integer.parseInt(lineArray[colIndex]);
-                                        telemetry.put("State", state);
-                                        break;
-                                    case "Timestamp":
-                                        int time = Integer.parseUnsignedInt(lineArray[colIndex]);
-                                        telemetry.put("Timestamp", time);
-                                        break;
-                                    default:
-                                        float out = Float.parseFloat(lineArray[colIndex]);
-                                        telemetry.put(idArray[colIndex], out);
-                                }
+
+                        for(int i=0; i < lineArray.length; i++) {
+                            String lineData = lineArray[i].replaceAll("\"(.*?)\"", "$1");
+                            String idData = idArray[i].replaceAll("\"(.*?)\"", "$1");
+                            if(isInteger(lineData)) {
+                                int intData = Integer.parseInt(lineData);
+
+                                telemetry.put(idData, intData);
+                            } else if(isFloat(lineData)) {
+                                float floatData = Float.parseFloat(lineData);
+
+                                telemetry.put(idData, floatData);
+                            } else {
+
+                                telemetry.put(idData, lineData);
                             }
-
-    
-    
-    
-    
-    
-    
-                            String telemetryJson = mapper.writeValueAsString(telemetry);
-    
-                            wss.broadcast(telemetryJson);
-                        } catch(NumberFormatException e) {
-
                         }
-                    
-                    Thread.sleep(10);
 
-                }
+                        telemetry.put("RocketConnected", true);
+
+                        String telemetryJson = mapper.writeValueAsString(telemetry);
+                        wss.broadcast(telemetryJson);
+
+                        Thread.sleep(100);
+
+                    }
                 }
                 
 
@@ -450,5 +413,23 @@ public class SerialManager implements SerialPortEventListener {
                 .sorted(Map.Entry.comparingByValue())
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2,
                         LinkedHashMap::new));
+    }
+
+    public static boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static boolean isFloat(String s) {
+        try {
+            Float.parseFloat(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
